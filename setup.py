d029149a7e8a45cd459c99a58fe36eacb0daac19
Pierre-Antoine HIGNARD <pa.hignard@pm.me>
2025-12-17 11:59:50 +0100

    updated requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,13 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         'numpy>=1.19.0',
-        # 'torch>=1.9.0',
-        # 'torchvision>=0.10.0',
+        'torch>=1.9.0',
+        'torchvision>=0.10.0',
+        'tqdm>=4.61.0',
+        'matplotlib>=3.4.0',
+        'seaborn>=0.11.0',
+        'scikit-learn>=0.24.0',
+        'pandas>=1.3.0',
+        'Pillow>=8.3.0',
     ],
 )
